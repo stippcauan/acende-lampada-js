fd530e858998732ea0067cf016e89eaa23584231
Cauan stipp <cauan.stipp@escola.pr.gov.br>
2025-10-23 12:17:09 +0000

    começado

---
+++ script.js
@@ -0,0 +1,9 @@
+function acerder(){
+    //document.getElementeById("Lampada").src="on.png"
+    console.log("On")
+}
+
+function apagar(){
+    //document.getElementeById("Lampada").src="off.png"
+    console.log("Off")
+}
